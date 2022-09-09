59d6dcc4d7a13b466290c7e01ef64265fdbad552
iamcyruss <russnicosia@gmail.com>
2022-09-09 06:28:46 -0600

    pretty much got step one working.

--- venv/main.py
+++ venv/main.py
@@ -17,25 +17,43 @@ TICKER_PARAMS = {
     "datatype": "json",
     "apikey": TICKER_DATA_API,
 }
+
+# get yesterday and day after from datetime
 current_date = datetime.now()
 print(current_date.today())
-today = str(current_date.today()).split()
 yesterday = str(current_date - timedelta(1)).split()
-print(today)
+day_after = str(current_date - timedelta(2)).split()
 print(yesterday)
+print(day_after)
 
+# get ticker data by date
 ticker_response = requests.get(TICKER_ENDPOINT, params=TICKER_PARAMS)
 ticker_response.raise_for_status()
 ticker_data = ticker_response.json()["Time Series (Daily)"]
+yesterdays_ticker_data = ticker_data[yesterday[0]]
+day_after_ticker_data = ticker_data[day_after[0]]
 print(ticker_data)
-print(ticker_data[today[0]])
-print(ticker_data[yesterday[0]])
-
 print(TICKER_PARAMS)
 print(f"ACCOUNT: {SMS_ACCOUNT_SID} and AUTH: {SMS_AUTH_TOKEN}")
 
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+def get_stock_data():
+    if yesterdays_ticker_data and day_after_ticker_data:
+        print(yesterdays_ticker_data)
+        print(day_after_ticker_data)
+        print(day_after_ticker_data['1. open'])
+        print(yesterdays_ticker_data['1. open'])
+        day_after_five = (float(day_after_ticker_data['1. open']) * 5.0) * .01
+        print(float(day_after_ticker_data['1. open']) - day_after_five)
+        print(float(day_after_ticker_data['1. open']) + day_after_five)
+        if day_after_ticker_data['1. open'] < yesterdays_ticker_data['1. open']:
+            if float(day_after_ticker_data['1. open']) - day_after_five < float(yesterdays_ticker_data['1. open']):
+                print('it went down more than 5%')
+        elif day_after_ticker_data['1. open'] < yesterdays_ticker_data['1. open']:
+            if float(day_after_ticker_data['1. open']) + day_after_five < float(yesterdays_ticker_data['1. open']):
+                print('it went up more than 5%')
+
 
 ## STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
